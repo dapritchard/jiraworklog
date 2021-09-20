d208f37003ef0ea7583f184b5dc7174dba54b44b
David Pritchard <david.al.pritchard@gmail.com>
2021-09-20 13:26:48 -0400

    add 'update_remote_worklogs'

--- jira_worklog.py
+++ jira_worklog.py
@@ -1,10 +1,18 @@
 #!/usr/bin/env python3
 
+import json
+# from jira import JIRA
+
+def update_remote_worklogs(jira, issue_nms, path):
+    with open(path, "w") as file:
+        worklogs_dict = fetch_worklogs(jira, issue_nms)
+        json.dump(worklogs_dict, file, indent=4)
+
 # Why does `jira.worklogs` require a network call? It seems like the issue
 # already contains all of the data
 def fetch_worklogs(jira, issue_nms):
     issues = fetch_issues(jira, issue_nms)
-    worklogs = {(x.key, x.id): jira.worklogs(x) for x in issues}
+    worklogs = {(x.key + "@" + x.id): jira.worklogs(x) for x in issues}
     return fetch_worklogs_impl(worklogs)
 
 def fetch_worklogs_impl(worklogs):
