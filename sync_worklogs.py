8afa1abb5213b29a147b58e96c1ec9a3b655406e
David Pritchard <david.al.pritchard@gmail.com>
2021-10-02 17:13:04 -0400

    updates to sync_worklogs.py

--- sync_worklogs.py
+++ sync_worklogs.py
@@ -46,6 +46,7 @@ def sync_worklogs_impl(worktree, head, remote):
                 head_nrm[k] = []
         return head_nrm
 
+
     # def add_issues(worklogs, issue_namekeys):
     #     return None
 
@@ -66,19 +67,62 @@ def sync_worklogs_impl(worktree, head, remote):
     head_nrm = normalize_head(head, issue_keys_worktree)
     remote_nrm = {extract_issue_key(k): v for (k, v) in remote.items()}
 
-    altrep_worktree = issues_to_altrep(worktree_nrm)
-    altrep_head = issues_to_altrep(head_nrm)
-    altrep_remote = issues_to_altrep(remote_nrm)
+    altrep_worktree = issues_to_altrep(worktree_nrm, False)
+    altrep_head = issues_to_altrep(head_nrm, True)
+    altrep_remote = issues_to_altrep(remote_nrm, True)
 
-def issues_to_altrep(issues):
+    altissue_keys = (list(altrep_worktree.keys())
+                     + list(altrep_head.keys())
+                     + list(altrep_remote.keys()))
+    unique_altissue_keys = list(set(altissue_keys))
+
+    # TODO: not this simple b/c have to think about what to do if data is in an
+    # inconsistent state (i.e. something changed on the remote not reflected in
+    # head or worktree)
+    altrep_merged = {}
+    for k in unique_altissue_keys:
+        altrep_merged[k] = create_merged_entries(
+            altrep_worktree.get(k, []),
+            altrep_head.get(k, []),
+            altrep_remote.get(k, [])
+        )
+
+    # return [worktree_nrm, head_nrm, remote_nrm]
+    return [altrep_worktree, altrep_head, altrep_remote]
+
+
+def extract_issue_keys(issues):
+    # [extract_issue_key(k) for k in issues.keys()]
+    return [k for k in issues.keys()]
+
+
+# It is assumed that the dict keys in `worklogs` are of the form `'name@key'`,
+# so this function returns a list of the `"key"` portion of each dict key string
+def extract_issue_key(dictkey):
+    return dictkey.split('@')[-1]
+
+
+def issues_to_altrep(issues, has_metadata):
+    create_value = create_metadata_dict if has_metadata else lambda _: {}
     def worklogs_to_dictrepr(worklogs):
-        return {create_core_tuple(w): create_metadata_dict(w) for w in worklogs}
-    {k: worklogs_to_dictrepr(v) for (k, v) in issues.items()}
+        return {create_core_tuple(w): create_value(w) for w in worklogs}
+    altrep = {}
+    for (key, worklogs) in issues.items():
+        for worklog in worklogs:
+            new_key = create_core_tuple(worklog, key)
+            new_value = create_value(worklog)
+            if new_key in altrep:
+                altrep[new_key].append(new_value)
+            else:
+                altrep[new_key] = [new_value]
+    return altrep
 
 
-def create_core_tuple(worklog):
-    keys = ['comment', 'started', 'timeSpentSeconds']
-    return tuple(worklog[k] for k in keys)
+def create_core_tuple(worklog, key):
+    return (key,
+            worklog['comment'],
+            worklog['started'],
+            worklog['timeSpentSeconds'])
 
 
 def create_metadata_dict(worklog):
@@ -93,107 +137,101 @@ def create_metadata_dict(worklog):
 
 
 
-def create_dictkey_canon(worktree_dictkeys, head_dictkeys, remote_dictkeys):
+# def create_dictkey_canon(worktree_dictkeys, head_dictkeys, remote_dictkeys):
 
-    # Check if the remote issue ID string corresponds to the local issue ID
-    # string. `remote_str` is of the form `"name@key"`, while `"local_str"` may
-    # either be of the form `"name"` or `"name@key"`. In the former case compare
-    # the `"name"` portions against each other, while in the latter case compare
-    # the `"key"` portions against each other.
-    def check_match(remote_dictkey, local_dictkey):
-        remote_split_str = remote_dictkey.split("@")
-        local_split_str = local_dictkey.split("@")
-        has_local_key = (len(local_split_str) >= 1)
-        return (
-            remote_split_str[1] == local_split_str[1]
-            if has_local_key
-            else remote_split_str[0] == local_dictkey)
+#     # Check if the remote issue ID string corresponds to the local issue ID
+#     # string. `remote_str` is of the form `"name@key"`, while `"local_str"` may
+#     # either be of the form `"name"` or `"name@key"`. In the former case compare
+#     # the `"name"` portions against each other, while in the latter case compare
+#     # the `"key"` portions against each other.
+#     def check_match(remote_dictkey, local_dictkey):
+#         remote_split_str = remote_dictkey.split("@")
+#         local_split_str = local_dictkey.split("@")
+#         has_local_key = (len(local_split_str) >= 1)
+#         return (
+#             remote_split_str[1] == local_split_str[1]
+#             if has_local_key
+#             else remote_split_str[0] == local_dictkey)
 
-    # Return the dict key in `local_dictkeys` corresponding to `remote_dictkey`
-    # if one can be found, or `None` if there is no match
-    def find_dictkey(remote_dictkey, local_dictkeys):
-        # https://stackoverflow.com/a/9868665/5518304
-        return next(
-            x for x in local_dictkeys if not check_match(remote_dictkey, x),
-            None)
+#     # Return the dict key in `local_dictkeys` corresponding to `remote_dictkey`
+#     # if one can be found, or `None` if there is no match
+#     def find_dictkey(remote_dictkey, local_dictkeys):
+#         # https://stackoverflow.com/a/9868665/5518304
+#         return next(
+#             x for x in local_dictkeys if not check_match(remote_dictkey, x),
+#             None)
 
-    local_dictkeys = list(set(worktree_dictkeys + head_dictkeys))
-    dictkey_map = {k: find_dictkey(k, remote_dictkeys) for k in local_dictkeys}
-    # # TODO: do we need this?
-    # for k in remote_dictkeys:
-    #     dictkey_map[k] = k
-    return dictkey_map
-
-
-
-# Make sure that the same set of issues are found in each dict with the same
-# keys
-#
-# The dict keys for `worktree` and possibly also for `head` for a given issue
-# may differ from the dict key for `remote` for the corresponding issue for one
-# of the following reasons.
-#     (i)  Only the issue name was provided for the working tree, so we need to
-#          append the issue key to the dict key.
-#     (ii) We already had both the issue name and the key, but now the name has
-#          changed on the remote so we need to update the dict key.
-# This is assumed to be a complete enumeration of the scenarios that could cause
-# the issue keys to differ.
-#
-# The issues in `remote` are assumed to be the full set of issues that we care
-# about since we grabbed them based on what issues are in `worktree`, and
-# furthermore the issue names and keys in the remote are the source of truth so
-# we simply need to make the keys for any corresponding issues in `worktree` and
-# `head` align with those.
-#
-# `head` might (i) have issues that aren't in `remote`, and it might also (ii)
-# be missing issues that are in remote. Any issues in `head` that are no longer
-# in `remote` (and by extension in `worktree` since the set of issues for those
-# two objects are assumed to be equal) are no longer needed and are therefore
-# removed. If we are missing issues in `head` that are in remote then we add
-# them with an empty worklog list.
-def align_issues(worktree, head, remote):
-
-    # Check if the remote issue ID string corresponds to the local issue ID
-    # string. `remote_str` is of the form `"name@key"`, while `"local_str"` may
-    # either be of the form `"name"` or `"name@key"`. In the former case compare
-    # the `"name"` portions against each other, while in the latter case compare
-    # the `"key"` portions against each other.
-    def check_match(remote_str, local_str):
-        remote_split_str = remote_str.split("@")
-        local_split_str = local_str.split("@")
-        has_local_key = (len(local_split_str) >= 1)
-        return (
-            remote_split_str[1] == local_split_str[1]
-            if has_local_key
-            else remote_split_str[0] == local_str)
-
-    # Return the dict key in `local_dictkeys` corresponding to `remote_dictkey`
-    # if one can be found, or `None` if there is no match
-    def find_dictkey(remote_dictkey, local_dictkeys):
-        # https://stackoverflow.com/a/9868665/5518304
-        return next(
-            x for x in local_dictkeys if not check_match(remote_dictkey, x),
-            None)
-
-    # Create a new dict with elements populated by the issues in `issues`
-    # corresponding to the dict keys in `dictkeys`. Note that this has two
-    # effects: (i) updating the keys in `issues`, and (ii) dropping any issues
-    # that don't correspond to a key in `dictkeys`
-    #
-    # There's surely a more efficient algorithm that can be used to
-    # perform this, but it may be the case that for the sizes of data we expect
-    # to encounter that this will be fast enough
-    def update_dictkeys(issues, dictkeys):
-        return {
-            k: issues.get(find_dictkey(k, dictkeys), []) for k in dictkeys}
-
-    remote_dictkeys = remote.keys()
-    worktree_newdictkeys = update_dictkeys(worktree, remote_dictkeys)
-    head_newdictkeys = update_dictkeys(head, remote_dictkeys)
-    return [worktree_newdictkeys, head_newdictkeys]
+#     local_dictkeys = list(set(worktree_dictkeys + head_dictkeys))
+#     dictkey_map = {k: find_dictkey(k, remote_dictkeys) for k in local_dictkeys}
+#     # # TODO: do we need this?
+#     # for k in remote_dictkeys:
+#     #     dictkey_map[k] = k
+#     return dictkey_map
 
 
-# It is assumed that the dict keys in `worklogs` are of the form `'name@key'`,
-# so this function returns a list of the `"key"` portion of each dict key string
-def extract_issue_key(dictkey):
-    return dictkey.split('@')[-1]
+
+# # Make sure that the same set of issues are found in each dict with the same
+# # keys
+# #
+# # The dict keys for `worktree` and possibly also for `head` for a given issue
+# # may differ from the dict key for `remote` for the corresponding issue for one
+# # of the following reasons.
+# #     (i)  Only the issue name was provided for the working tree, so we need to
+# #          append the issue key to the dict key.
+# #     (ii) We already had both the issue name and the key, but now the name has
+# #          changed on the remote so we need to update the dict key.
+# # This is assumed to be a complete enumeration of the scenarios that could cause
+# # the issue keys to differ.
+# #
+# # The issues in `remote` are assumed to be the full set of issues that we care
+# # about since we grabbed them based on what issues are in `worktree`, and
+# # furthermore the issue names and keys in the remote are the source of truth so
+# # we simply need to make the keys for any corresponding issues in `worktree` and
+# # `head` align with those.
+# #
+# # `head` might (i) have issues that aren't in `remote`, and it might also (ii)
+# # be missing issues that are in remote. Any issues in `head` that are no longer
+# # in `remote` (and by extension in `worktree` since the set of issues for those
+# # two objects are assumed to be equal) are no longer needed and are therefore
+# # removed. If we are missing issues in `head` that are in remote then we add
+# # them with an empty worklog list.
+# def align_issues(worktree, head, remote):
+
+#     # Check if the remote issue ID string corresponds to the local issue ID
+#     # string. `remote_str` is of the form `"name@key"`, while `"local_str"` may
+#     # either be of the form `"name"` or `"name@key"`. In the former case compare
+#     # the `"name"` portions against each other, while in the latter case compare
+#     # the `"key"` portions against each other.
+#     def check_match(remote_str, local_str):
+#         remote_split_str = remote_str.split("@")
+#         local_split_str = local_str.split("@")
+#         has_local_key = (len(local_split_str) >= 1)
+#         return (
+#             remote_split_str[1] == local_split_str[1]
+#             if has_local_key
+#             else remote_split_str[0] == local_str)
+
+#     # Return the dict key in `local_dictkeys` corresponding to `remote_dictkey`
+#     # if one can be found, or `None` if there is no match
+#     def find_dictkey(remote_dictkey, local_dictkeys):
+#         # https://stackoverflow.com/a/9868665/5518304
+#         return next(
+#             x for x in local_dictkeys if not check_match(remote_dictkey, x),
+#             None)
+
+#     # Create a new dict with elements populated by the issues in `issues`
+#     # corresponding to the dict keys in `dictkeys`. Note that this has two
+#     # effects: (i) updating the keys in `issues`, and (ii) dropping any issues
+#     # that don't correspond to a key in `dictkeys`
+#     #
+#     # There's surely a more efficient algorithm that can be used to
+#     # perform this, but it may be the case that for the sizes of data we expect
+#     # to encounter that this will be fast enough
+#     def update_dictkeys(issues, dictkeys):
+#         return {
+#             k: issues.get(find_dictkey(k, dictkeys), []) for k in dictkeys}
+
+#     remote_dictkeys = remote.keys()
+#     worktree_newdictkeys = update_dictkeys(worktree, remote_dictkeys)
+#     head_newdictkeys = update_dictkeys(head, remote_dictkeys)
+#     return [worktree_newdictkeys, head_newdictkeys]
