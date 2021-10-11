98318aeb4ce9a3eac1e84678b6fc5f9566baf36d
David Pritchard <david.al.pritchard@gmail.com>
2021-10-10 21:21:47 -0400

    update compare_remote_changes.py

--- compare_remote_changes.py
+++ compare_remote_changes.py
@@ -32,28 +32,44 @@ def check_remote_changes(altrep_head, altrep_remote):
             modifications['updated'].append([idrep_head[id], idrep_remote[id]])
 
     subgraphs = {}
-    for update in modifications['updated']:
-        subgraph_keys = subgraphs.keys()
-        membersof = [k for k in subgraph_keys if check_in(update['keys'], k)]
-        if len(membersof) == 0:
-            subgraphs[tuple(update)] = [update]
-        elif len(memborsof) == 1:
-            # FIXME: THIS ISN"T RIGHT! Case: either the 'from' or 'to' worklogs are already present in a
-            # subgraph, but not both
-            if len(update) == 2:
-                oldkey = membersof[0]
-                newkey_elem = update[0] if update[0] in oldkey else update[1]
-                newkey = (*oldkey, newkey_elem)
-                newvalue = subgraphs[oldkey].append(update)
-                del subgraphs[oldkey]
+    for modkey in ['added', 'updated', 'removed']:
+        for altkeys in modifications[modkey]:
+            subgraph_keys = subgraphs.keys()
+            membersof = [k for k in subgraph_keys if check_in(altkeys, k)]
+            if len(membersof) == 0:
+                subgraphs[tuple(altkeys)] = {
+                    'added': [],
+                    'updated': [],
+                    'removed': []
+                }
+                subgraphs[tuple(altkeys)][modkey].append(altkeys)
+            elif len(altkeys) == 1:
+                subgraphs[membersof[0]][modkey].append(altkeys)
+            elif len(membersof) == 2:
+                subgraph_0 = subgraphs[membersof[0]]
+                subgraph_1 = subgraphs[membersof[1]]
+                newkey = (*membersof[0], *membersof[1])
+                newval = {
+                    'added': subgraph_0['added'] ++ subgraph_1['added'],
+                    'updated': subgraph_0['updated'] ++ subgraph_1['updated'],
+                    'removed': subgraph_0['removed'] ++ subgraph_1['removed']
+                }
+                newval[modkey].append(altkeys)
+                del subgraphs[membersof[0]]
+                del subgraphs[membersof[1]]
+                subgraphs[newkey] = newval
+            elif altkeys[0] in membersof[0] and altkeys[1] in membersof[0]:
+                subgraphs[membersof[0]][modkey].append(altkeys)
+            # Case:  either the 'from' or 'to' worklogs are already present
+            # in a subgraph, but not both
+            else:
+                newkey_elem = (altkeys[0]
+                                if altkeys[0] in membersof[0]
+                                else altkeys[1])
+                newkey = (*membersof[0], newkey_elem)
+                newvalue = subgraphs[membersof[0]][modkey].append(altkeys)
+                del subgraphs[membersof[0]]
                 subgraphs[newkey] = newvalue
-            # Case:
-            else
-                pass
-        else
-            pass
-
-        pass
 
 
 # Create a dict with keys the worklog ID and values the "altrep" representation
