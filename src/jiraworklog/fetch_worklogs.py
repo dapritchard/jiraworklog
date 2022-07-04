3e9c61d0d30d974ff37d90894abd24eb5632f9d6
David Pritchard <david.al.pritchard@gmail.com>
2022-07-04 15:00:41 -0400

    add read_jira_worklogs

--- src/jiraworklog/fetch_worklogs.py
+++ src/jiraworklog/fetch_worklogs.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 
 import json
-# from jira import JIRA
 
+from jiraworklog.configuration import *
+
+# TODO: remove this?
 def fetch_worklogs(jira, issue_nms, path):
     if not isinstance(issue_nms, list):
         raise Exception("'issue_nms' must be a list")
@@ -14,6 +16,7 @@ def fetch_worklogs(jira, issue_nms, path):
         file.write("\n")
     return worklogs_dict
 
+# TODO: remove this?
 # Why does `jira.worklogs` require a network call? It seems like the issue
 # already contains all of the data
 def fetch_worklogs_remotedata(jira, issue_nms):
@@ -23,6 +26,14 @@ def fetch_worklogs_remotedata(jira, issue_nms):
     # https://community.atlassian.com/t5/Agile-questions/Unique-Issue-ID-where-do-we-stand/qaq-p/586280?tempId=eyJvaWRjX2NvbnNlbnRfbGFuZ3VhZ2VfdmVyc2lvbiI6IjIuMCIsIm9pZGNfY29uc2VudF9ncmFudGVkX2F0IjoxNjMyMTU0MzIzNDMxfQ%3D%3D
     worklogs = {(x.key + "@" + x.id): jira.worklogs(x) for x in issues}
     return {k: [extract_worklog_fields(x) for x in v] for (k, v) in worklogs.items()}
+
+def read_jira_worklogs(jira, conf):
+    issues = {nm: jira.issue(nm) for nm in conf_jira_issue_nms(conf)}
+    worklogs = {k: jira.worklogs(v) for (k, v) in issues.items()}
+    nrm_worklogs = {k: [extract_worklog_fields(x) for x in v]
+                    for (k, v)
+                    in worklogs.items()}
+    return nrm_worklogs
 
 def extract_worklog_fields(worklog):
     raw = worklog.raw
