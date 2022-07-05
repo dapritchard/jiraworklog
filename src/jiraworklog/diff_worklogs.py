ebf03dac6db63d603fcc8e6c86424e1556ac95c0
David Pritchard <david.al.pritchard@gmail.com>
2022-07-05 09:10:16 -0400

    a few tweaks to local diffing

--- src/jiraworklog/diff_worklogs.py
+++ src/jiraworklog/diff_worklogs.py
@@ -3,6 +3,9 @@
 def diff_local_worklogs(local, checkedin):
     pass
 
+# The efficiency of this algorithm could likely by improved. However, note that
+# we have to handle the possibility of duplicate worklog entries which precludes
+# us from doing certain things like using sets
 def diff_issue_worklogs(issue_local, issue_checkedin):
     def minimize(w):
         return {
@@ -10,16 +13,17 @@ def diff_issue_worklogs(issue_local, issue_checkedin):
             'started': w['started'],
             'timeSpentSeconds': w['timeSpentSeconds']
         }
-    augm_checkedin = [(minimize(v), v) for v in issue_checkedin]
+    issue_augchk = [{'minimal': minimize(v), 'full': v}
+                    for v
+                    in issue_checkedin]
     added = []
-    for w in issue_local:
-        for i, a in enumerate(augm_checkedin):
-            if w == a[0]:
-                augm_checkedin.pop(i)
-                both.append
+    for wkl_local in issue_local:
+        for i, wkl_augchk in enumerate(issue_augchk):
+            if wkl_local == wkl_augchk['minimal']:
+                issue_augchk.pop(i)
                 continue
-        added.append(w)
+        added.append(wkl_local)
     return {
         'added': added,
-        'removed': augm_checkedin
+        'removed': issue_augchk
     }
