57743dccdaa59c6ea635cb4f1da4be5835194a56
David Pritchard <david.al.pritchard@gmail.com>
2022-07-10 19:59:46 -0400

    update instuction creation

--- src/jiraworklog/reconcile_external.py
+++ src/jiraworklog/reconcile_external.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python3
 
-def create_update_instructions(diff_local, diff_remote):
-    nested_update_instrs = reconcile_external_changes(diff_local, diff_remote)
+def create_update_instructions(diffs_local, diffs_remote):
+    # TODO: assert that keys are identical?
+    nested_update_instrs = {
+        k: reconcile_external_changes(diffs_local[k], diffs_remote[k])
+        for k
+        in diffs_local.keys()
+    }
     update_instrs = flatten_update_instructions(nested_update_instrs)
     return update_instrs
 
@@ -24,10 +29,10 @@ def reconcile_external_changes(diff_local, diff_remote):
 
 def flatten_update_instructions(nested_diffs):
     flattened = []
-    for k_where, v_where in nested_diffs.items():
-        for k_action, v_action in v_where.items():
-            for k_issue, v_issue in v_action.items():
-                for augwkl in v_issue:
+    for k_issue, v_issue in nested_diffs.items():
+        for k_where, v_where in v_issue.items():
+            for k_action, v_action in v_where.items():
+                for augwkl in v_action:
                     entry = {
                         'remote': True if k_where == 'local' else False,
                         'action': k_action,
