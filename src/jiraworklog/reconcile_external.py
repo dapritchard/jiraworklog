a7674cfb8f95d0d5d48f0094d1cfb1ecd7facb93
David Pritchard <david.al.pritchard@gmail.com>
2022-07-10 13:40:42 -0400

    rearrange file contents

--- src/jiraworklog/reconcile_external.py
+++ src/jiraworklog/reconcile_external.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
 
+from jiraworklog.delete_worklog import delete_worklog
+
+
 def update_worklogs(jira, checkedin, diff_local, diff_remote):
     # Note that `checkedin` is modified in the call to `perform_update_actions`
     update_instrs = create_update_instructions(diff_local, diff_remote)
@@ -18,25 +21,22 @@ def perform_update_actions(jira, checkedin, instructions):
         update_checkedin(checkedin, **instruction)
     return checkedin
 
-def update_checkedin(checkedin, action, issue, augwkl):
-    assert action in ['added', 'removed']
-    if action == 'added':
-        checkedin[issue].append(augwkl['full'])
-    else:
-        found_match = False
-        for i, augwkl_checkedin in enumerate(checkedin):
-            if augwkl_checkedin['canon'] == augwkl['canon']:
-                found_match = True
-                checkedin.pop(i)
-                continue
-        if not found_match:
-            raise RuntimeError('Internal logic error. Please file a bug report')
-
-def update_remote(jira, action, issue, augwkl):
-    pass
-
-def update_remote_single(jira, action, issue, worklog):
-    pass
+def reconcile_external_changes(diff_local, diff_remote):
+    added = find_aligned_extchanges(diff_local['added'], diff_remote['added'])
+    removed = find_aligned_extchanges(
+        diff_local['removed'],
+        diff_remote['removed']
+    )
+    return {
+        'checkedin': {
+            'added': added['aligned'],
+            'removed': removed['aligned']
+        },
+        'local': {
+            'added': added['local'],
+            'removed': removed['local']
+        }
+    }
 
 def flatten_update_instructions(diffs):
     flattened = []
@@ -53,34 +53,25 @@ def flatten_update_instructions(diffs):
                     flattened.append(entry)
     return flattened
 
-def add_checkedin(iss_checkedin, iss_key, augwkl_local):
-    found_match = False
-    for i, augwkl_checkedin in enumerate(iss_checkedin[iss_key]):
-        if augwkl_checkedin['canon'] == augwkl_local['canon']:
-            found_match = True
-            iss_checkedin.pop(i)
-            continue
-    if not found_match:
-        raise RuntimeError('Internal logic error. Please file a bug report')
+def update_checkedin(checkedin, action, issue, augwkl):
+    assert action in ['added', 'removed']
+    if action == 'added':
+        # TODO: consider sorting by start time?
+        checkedin[issue].append(augwkl['full'])
+    else:
+        found_match = False
+        for i, augwkl_checkedin in enumerate(checkedin):
+            if augwkl_checkedin['canon'] == augwkl['canon']:
+                found_match = True
+                checkedin.pop(i)
+                continue
+        if not found_match:
+            raise RuntimeError('Internal logic error. Please file a bug report')
 
-def reconcile_external_changes(diff_local, diff_remote):
-    added = find_aligned(diff_local['added'], diff_remote['added'])
-    removed = find_aligned(diff_local['removed'], diff_remote['removed'])
-    return {
-        'checkedin': {
-            'added': added['aligned'],
-            'removed': removed['aligned']
-        },
-        'local': {
-            'added': added['local'],
-            'removed': removed['local']
-        }
-    }
-
-def find_aligned(diff_local, diff_remote):
+def find_aligned_extchanges(diff_local, diff_remote):
     aligned = []
     updated_local = []
-    for augwkl_local in worklogs_local:
+    for augwkl_local in diff_local:
         found_match = False
         for i, augwkl_remote in enumerate(diff_remote):
             if augwkl_remote['canon'] == augwkl_local['canon']:
@@ -90,9 +81,30 @@ def find_aligned(diff_local, diff_remote):
                 continue
         if not found_match:
             updated_local.append(augwkl_local)
-    # return [updated_local, diff_remote, aligned]
     return {
         'local': updated_local,
         'remote': diff_remote,
         'aligned': aligned
     }
+
+def update_remote(jira, action, issue, augwkl):
+    wkl = augwkl['canon']
+    assert action in ['added', 'removed']
+    if action == 'added':
+        jira.add_worklog(
+            issue=issue,
+            timeSpent=wkl['timeSpent'],
+            comment=wkl['comment']
+        )
+    else:
+        delete_worklog(jira=jira, issue=issue)
+
+# def add_checkedin(iss_checkedin, iss_key, augwkl_local):
+#     found_match = False
+#     for i, augwkl_checkedin in enumerate(iss_checkedin[iss_key]):
+#         if augwkl_checkedin['canon'] == augwkl_local['canon']:
+#             found_match = True
+#             iss_checkedin.pop(i)
+#             continue
+#     if not found_match:
+#         raise RuntimeError('Internal logic error. Please file a bug report')
