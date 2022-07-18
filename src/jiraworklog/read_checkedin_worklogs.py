b9f7487a5e2c21f47a099cbf9ac708349e1414d0
David Pritchard <david.al.pritchard@gmail.com>
2022-07-17 21:57:48 -0400

    update read_checkedin_worklogs.py

--- src/jiraworklog/read_checkedin_worklogs.py
+++ src/jiraworklog/read_checkedin_worklogs.py
@@ -1,32 +1,41 @@
 #!/usr/bin/env python3
 
 import json
-import os.path
 from jiraworklog.configuration import Configuration
+from jiraworklog.diff_worklogs import map_worklogs
+from jiraworklog.worklogs import WorklogCheckedin
+import os.path
+from typing import Any, Dict, List
 
-def read_checkedin_worklogs(conf):
-    if 'checked_in_path' in conf and not conf['checked_in_path'] is None:
-        is_default_path = False
-        raw_path = conf['checked_in_path']
-        checkedin_path = os.path.expanduser(raw_path)
-    else:
+def read_checkedin_worklogs(
+    conf: Configuration
+) -> Dict[str, List[WorklogCheckedin]]:
+    if conf.checked_in_path is None:
         is_default_path = True
         raw_path = ''
         checkedin_path = os.path.expanduser(
             '~/.config/jira-worklog/checked-in-worklogs.json'
         )
+    else:
+        is_default_path = False
+        raw_path = conf['checked_in_path']
+        checkedin_path = os.path.expanduser(raw_path)
     try:
         with open(checkedin_path) as checkedin_file:
             # TODO: validate contents
-            worklogs = json.load(checkedin_file)
+            worklogs_raw = json.load(checkedin_file)
     except:
         # TODO: query user whether we should start a new file. For now we just
         # unconditionally do so
-        worklogs = {}
-    nrm_worklogs = align_checkedin_with_conf(worklogs, conf)
-    return nrm_worklogs
+        worklogs_raw = {}
+    align_checkedin_with_conf(worklogs_raw, conf)
+    worklogs = map_worklogs(WorklogCheckedin, worklogs_raw)
+    return worklogs
 
-def align_checkedin_with_conf(worklogs, conf: Configuration):
+def align_checkedin_with_conf(
+    worklogs: Dict[str, Any],
+    conf: Configuration
+) -> Dict[str, Any]:
     conf_nms = set(conf.issue_nms)
     checkedin_nms = set(worklogs.keys())
     added_nms = conf_nms - checkedin_nms
@@ -35,6 +44,6 @@ def align_checkedin_with_conf(worklogs, conf: Configuration):
         worklogs[nm] = []
     if len(removed_nms) >= 1:
         # TODO: query user before removing
-        for nm in added_nms:
+        for nm in removed_nms:
             del worklogs[nm]
     return worklogs
