c64fc0a83fe7e2730fa6079afafe28fe9b9b5b2d
David Pritchard <david.al.pritchard@gmail.com>
2022-07-21 21:23:49 -0400

    update type hints for read_remote_worklogs

--- src/jiraworklog/read_remote_worklogs.py
+++ src/jiraworklog/read_remote_worklogs.py
@@ -4,8 +4,13 @@ from jira import JIRA
 from jiraworklog.configuration import Configuration
 from jiraworklog.worklogs import WorklogJira
 
-def read_remote_worklogs(jira: JIRA, conf: Configuration) -> dict[str, list[WorklogJira]]:
+def read_remote_worklogs(
+    jira: JIRA,
+    conf: Configuration
+) -> dict[str, list[WorklogJira]]:
     # TODO: error handling
     issues = {nm: jira.issue(nm) for nm in conf.issue_nms}
-    worklogs = {k: jira.worklogs(v) for (k, v) in issues.items()}
+    worklogs = {k: [WorklogJira(x) for x in jira.worklogs(v)]
+                for (k, v)
+                in issues.items()}
     return worklogs
