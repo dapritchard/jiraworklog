516de79ab358d3d99a70f01f91f88345047cf0fd
David Pritchard <david.al.pritchard@gmail.com>
2022-07-21 21:04:25 -0400

    comment out old routines

--- src/jiraworklog/push_worklogs.py
+++ src/jiraworklog/push_worklogs.py
@@ -128,50 +128,50 @@ def push_worklogs_NEW(
     chk_4 = reduce(push_worklog_remove, update_instrs.remote_remove, chk_3)
     return chk_4
 
-def push_worklogs(jira, checkedin, update_instrs):
-    for instr in update_instrs:
-        if instr.pop('remote'):
-            maybe_jira_wkl = update_remote(jira, **instr)
-            if maybe_jira_wkl is not None:
-                instr['augwkl'] = create_augwkl_jira(maybe_jira_wkl)
-        update_checkedin(checkedin, **instr)
+# def push_worklogs(jira, checkedin, update_instrs):
+#     for instr in update_instrs:
+#         if instr.pop('remote'):
+#             maybe_jira_wkl = update_remote(jira, **instr)
+#             if maybe_jira_wkl is not None:
+#                 instr['augwkl'] = create_augwkl_jira(maybe_jira_wkl)
+#         update_checkedin(checkedin, **instr)
 
-def update_checkedin(checkedin, action, issue, augwkl):
-    assert action in ['added', 'removed']
-    if action == 'added':
-        # TODO: consider sorting by start time?
-        checkedin[issue].append(augwkl)
-    else:
-        found_match = False
-        for i, augwkl_checkedin in enumerate(checkedin[issue]):
-            if augwkl_checkedin['canon'] == augwkl['canon']:
-                found_match = True
-                del checkedin[issue][i]
-                continue
-        if not found_match:
-            raise RuntimeError('Internal logic error. Please file a bug report')
+# def update_checkedin(checkedin, action, issue, augwkl):
+#     assert action in ['added', 'removed']
+#     if action == 'added':
+#         # TODO: consider sorting by start time?
+#         checkedin[issue].append(augwkl)
+#     else:
+#         found_match = False
+#         for i, augwkl_checkedin in enumerate(checkedin[issue]):
+#             if augwkl_checkedin['canon'] == augwkl['canon']:
+#                 found_match = True
+#                 del checkedin[issue][i]
+#                 continue
+#         if not found_match:
+#             raise RuntimeError('Internal logic error. Please file a bug report')
 
-def update_remote(jira, action, issue, augwkl):
-    assert action in ['added', 'removed']
-    if action == 'added':
-        canon = augwkl['canon']
-        maybe_jira_wkl = jira.add_worklog(
-            issue=issue,
-            timeSpentSeconds=canon['timeSpentSeconds'],
-            comment=canon['comment']
-        )
-    else:
-        full = augwkl['full']
-        jira_wkl = jira.worklog(full['issueId'], full['id'])
-        maybe_jira_wkl = jira_wkl.delete()
-    return maybe_jira_wkl
+# def update_remote(jira, action, issue, augwkl):
+#     assert action in ['added', 'removed']
+#     if action == 'added':
+#         canon = augwkl['canon']
+#         maybe_jira_wkl = jira.add_worklog(
+#             issue=issue,
+#             timeSpentSeconds=canon['timeSpentSeconds'],
+#             comment=canon['comment']
+#         )
+#     else:
+#         full = augwkl['full']
+#         jira_wkl = jira.worklog(full['issueId'], full['id'])
+#         maybe_jira_wkl = jira_wkl.delete()
+#     return maybe_jira_wkl
 
-# def add_checkedin(iss_checkedin, iss_key, augwkl_local):
-#     found_match = False
-#     for i, augwkl_checkedin in enumerate(iss_checkedin[iss_key]):
-#         if augwkl_checkedin['canon'] == augwkl_local['canon']:
-#             found_match = True
-#             iss_checkedin.pop(i)
-#             continue
-#     if not found_match:
-#         raise RuntimeError('Internal logic error. Please file a bug report')
+# # def add_checkedin(iss_checkedin, iss_key, augwkl_local):
+# #     found_match = False
+# #     for i, augwkl_checkedin in enumerate(iss_checkedin[iss_key]):
+# #         if augwkl_checkedin['canon'] == augwkl_local['canon']:
+# #             found_match = True
+# #             iss_checkedin.pop(i)
+# #             continue
+# #     if not found_match:
+# #         raise RuntimeError('Internal logic error. Please file a bug report')
