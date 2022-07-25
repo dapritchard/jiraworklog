daee358da37c09a7ee44ece7cd6c592ef7123683
David Pritchard <david.al.pritchard@gmail.com>
2022-07-25 16:39:59 -0400

    add commented-out __eq__ method for WorklogCheckedin

--- src/jiraworklog/worklogs.py
+++ src/jiraworklog/worklogs.py
@@ -40,6 +40,13 @@ class WorklogCheckedin(WorklogCanon):
         super().__init__(canon, issueKey)
         self.full = full
 
+    # def __eq__(self, obj: Any) -> bool:
+    #     if isinstance(obj, WorklogCheckedin):
+    #         out = self.full == obj.full and super().__eq__(obj)
+    #     else:
+    #         out = super().__eq__(obj)
+    #     return out
+
     def to_canon(self) -> WorklogCanon:
         return WorklogCanon(self.canon, self.issueKey)
 
