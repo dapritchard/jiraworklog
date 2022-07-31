5142cbba1c9ecb4f5bcc82ffae3955c1a5d1c038
David Pritchard <david.al.pritchard@gmail.com>
2022-07-31 16:04:11 -0400

    rework Jira mocking

--- tests/jiramock.py
+++ tests/jiramock.py
@@ -1,6 +1,52 @@
 #!/usr/bin/env python3
 
-class JIRAWklMock:
+import jira.resources as j
+from jiraworklog.worklogs import jira_to_full
+from typing import Any, Optional
+
+
+class JIRAMock:
+
+    path: str
+    entries: list[dict[str, dict[str, str]]]
+    curr_id: int
+
+    def __init__(
+        self,
+        path: str
+    ) -> None:
+        self.path = path
+
+    def add_worklog(
+        self,
+        issue: str,
+        timeSpentSeconds: str,
+        comment: str,
+        started: str
+    ):
+        wkl = {
+            'issue': issue,
+            'timeSpentSeconds': timeSpentSeconds,
+            'comment': comment,
+            'started': started
+        }
+        entry = {'action': 'add', 'worklog': wkl}
+        self.entries.append(entry)
+
+
+        # issue,
+        # timeSpent: (Optional[str]) = None,
+        # timeSpentSeconds: (Optional[str]) = None,
+        # adjustEstimate: (Optional[str]) = None,
+        # newEstimate: (Optional[str]) = None,
+        # reduceBy: (Optional[str]) = None,
+        # comment: (Optional[str]) = None,
+        # started: (Optional[datetime.datetime]) = None,
+        # user: (Optional[str]) = None,
+class JIRAWklMock(j.Worklog):
+
+    raw: dict[str, Any]
+    jira: Optional[JIRAMock]
 
     def __init__(
         self,
@@ -31,6 +77,15 @@ class JIRAWklMock:
             },
             'updated': updated
         }
+        self.jira = None
 
     def __repr__(self) -> str:
         return f"<JIRAMock Worklog: id='{self.raw['id']}'>"
+
+    def delete(self) -> None:
+        if self.jira is not None:
+            entry = {'action': 'remove', 'worklog': jira_to_full(self)}
+            self.jira.entries.append(entry)
+        else:
+            msg = 'Tried to call `delete` without setting `jira` attribute'
+            raise RuntimeError(msg)
