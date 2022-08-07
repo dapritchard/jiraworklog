252a0d80e4e3cf07f88b4e0dc5d5338f463e1317
David Pritchard <david.al.pritchard@gmail.com>
2022-08-07 17:24:57 -0400

    add --config-path argument

--- src/jiraworklog/cmdline_args.py
+++ src/jiraworklog/cmdline_args.py
@@ -2,5 +2,9 @@
 
 import argparse
 
+# https://docs.python.org/3/howto/argparse.html
+# https://docs.python.org/3/library/argparse.html
+# https://stackoverflow.com/a/18161115
 parser = argparse.ArgumentParser()
 parser.add_argument('file')
+parser.add_argument('--config-path')
