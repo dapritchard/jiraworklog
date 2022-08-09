44c1d03253199cae096dfc81b983bfd5f1480580
David Pritchard <david.al.pritchard@gmail.com>
2022-08-09 18:36:50 -0400

    add 01-empty-to-empty test

--- tests/test_application.py
+++ tests/test_application.py
@@ -2,5 +2,6 @@
 
 from tests.create_apptest import create_apptest
 
+test_01 = create_apptest('tests/data/01-empty-to-empty/')
 test_02 = create_apptest('tests/data/02-add-to-empty/')
 test_03 = create_apptest('tests/data/03-remove-to-empty/')
