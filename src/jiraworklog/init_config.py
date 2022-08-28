332fed63f74bcdbdf54b02bf9cf06fed2314bc83
David Pritchard <david.al.pritchard@gmail.com>
2022-08-28 04:45:02 -0400

    more config file queries

--- src/jiraworklog/init_config.py
+++ src/jiraworklog/init_config.py
@@ -14,6 +14,16 @@ def init_config_impl():
 
     textwrapper = TextWrapper(replace_whitespace=False)
 
+    msg = (
+        'What should the filepath be for the configuration file? (If you leave '
+        "this blank it will default to '.jiraconfig.yaml'.) "
+    )
+    print_para(msg, textwrapper)
+    config_path = input()
+    if config_path == '':
+        config_path = os.path.expanduser('~/.jwconfig.yaml')
+    # TODO: try to write empty file to see if we have permission
+
     print('What is your Jira author name? ', end='')
     author = input()
 
@@ -22,7 +32,8 @@ def init_config_impl():
     msg = (
         "What is the Jira server's URL? (You can leave this blank if you "
         "intend to supply this value via the JW_SERVER environmental "
-        "variable.) "    )
+        "variable.) "
+    )
     print_para(msg, textwrapper)
     server = input()
 
@@ -41,9 +52,30 @@ def init_config_impl():
     print_para(msg, textwrapper)
     api_token = input()
 
-    # print(
-    #     "What is the "
-    # )
+    # TODO: issues_map
+
+    # TODO: timezone. What happens if we upload without one?
+
+    msg = (
+        'What should the filepath be for the file that jiraworklog uses to '
+        'store its worklog records? (You can leave this blank if you intend to '
+        "supply the location via the '--config-file' command-line argument.) "
+    )
+    print_para(msg, textwrapper)
+    checked_in_path = input()
+
+    msg = (
+        'What kind of filetype will you use to provide your worklog records '
+        "with? (You must enter one of either 'csv' or 'excel'.)"
+    )
+    print_para(msg, textwrapper)
+    parse_type = input()
+    while not parse_type in ['csv', 'excel']:
+        msg = (
+            "Invalid filetype response. Please enter one of either 'csv' or "
+            "'excel': "
+        )
+        parse_type = input()
 
 
 def print_para(msg, textwrapper):
